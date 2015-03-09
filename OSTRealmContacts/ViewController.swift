5f277ec4fe8e26240a272d91487203cdae288b69
Robert Otani <robert@otanistudio.com>
2015-03-08 17:26:33 -0700

    Initial Commit

---
+++ OSTRealmContacts/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OSTRealmContacts
+//
+//  Created by Robert Otani on 3/8/15.
+//  Copyright (c) 2015 Robert Otani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
