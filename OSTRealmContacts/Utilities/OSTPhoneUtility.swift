69b7070fd5d6c0a02448a6c1687eda5e5e3d6ce9
Robert Otani <robert@otanistudio.com>
2015-03-15 22:09:11 -0700

    Helper utility for phones — hacked together super quick in objc, then clumsily ported to swift... this will need to be revisited so adheres to a saner swiftlike approach

---
+++ OSTRealmContacts/Utilities/OSTPhoneUtility.swift
@@ -0,0 +1,55 @@
+//
+//  OSTPhoneUtility.swift
+//  OSTRealmContacts
+//
+//  Created by Robert Otani on 3/15/15.
+//  Copyright (c) 2015 Robert Otani. All rights reserved.
+//
+
+import UIKit
+
+class OSTPhoneUtility: NSObject {
+    class func phoneLetters() -> [String:String] {
+        return [
+         "A": "2",
+         "B": "2",
+         "C": "2",
+         "D": "3",
+         "E": "3",
+         "F": "3",
+         "G": "4",
+         "H": "4",
+         "I": "4",
+         "J": "5",
+         "K": "5",
+         "L": "5",
+         "M": "6",
+         "N": "6",
+         "O": "6",
+         "P": "7",
+         "Q": "7",
+         "R": "7",
+         "S": "7",
+         "T": "8",
+         "U": "8",
+         "V": "8",
+         "W": "9",
+         "X": "9",
+         "Y": "9",
+         "Z": "9"
+        ]
+    }
+    
+    class func normalizedPhoneStringFromString(phoneString: NSString?) -> NSString {
+        let nonASCIICharacterSet = NSCharacterSet(charactersInString: "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ").invertedSet
+        var tmpSeparated = phoneString!.componentsSeparatedByCharactersInSet(nonASCIICharacterSet) as NSArray
+        let immutablePhoneString = tmpSeparated.componentsJoinedByString("") as NSString
+        var stringToFix = immutablePhoneString.mutableCopy() as NSMutableString
+        let range = NSMakeRange(0, stringToFix.length)
+        for (letter, tone) in phoneLetters() {
+            stringToFix.replaceOccurrencesOfString(letter, withString: tone, options:.CaseInsensitiveSearch, range: range)
+        }
+        return stringToFix.copy() as NSString
+    }
+    
+}
