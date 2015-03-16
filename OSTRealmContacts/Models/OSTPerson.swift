c1b06db54fdb093e9cee35b05d0f038f325368a8
Robert Otani <robert@otanistudio.com>
2015-03-15 22:13:38 -0700

    first draft of person rlmobject

---
+++ OSTRealmContacts/Models/OSTPerson.swift
@@ -0,0 +1,19 @@
+//
+//  OSTPerson.swift
+//  OSTRealmContacts
+//
+//  Created by Robert Otani on 3/15/15.
+//  Copyright (c) 2015 Robert Otani. All rights reserved.
+//
+
+import Foundation
+import Realm
+
+class OSTPerson : RLMObject {
+    dynamic var fullName = ""
+    dynamic var firstName = ""
+    dynamic var lastName = ""
+    dynamic var phoneNumber = ""
+    
+
+}
