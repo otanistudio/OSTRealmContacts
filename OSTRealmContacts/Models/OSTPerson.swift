eed839e10de9f4daac96fdde54efa0e9ac35d977
Robert Otani <robert@otanistudio.com>
2015-04-18 09:45:49 -0700

    Implement custom initializer

--- OSTRealmContacts/Models/OSTPerson.swift
+++ OSTRealmContacts/Models/OSTPerson.swift
@@ -15,5 +15,31 @@ class OSTPerson : RLMObject {
     dynamic var lastName = ""
     dynamic var phoneNumber = ""
     
+    override class func primaryKey() -> String {
+        return "fullName"
+    }
+    
+    init!(fullName: String, firstName: String, lastName: String, phoneNumber: String) {
+        super.init()
+        self.fullName = fullName
+        self.firstName = firstName
+        self.lastName = lastName
+        self.phoneNumber = phoneNumber
+    }
+    
+    override init() {
+        super.init()
+    }
 
+    override init!(object value: AnyObject!, schema: RLMSchema!) {
+        super.init(object: value, schema: schema)
+    }
+    
+    override init!(object: AnyObject!) {
+        super.init(object: object)
+    }
+    
+    override init!(objectSchema schema: RLMObjectSchema!) {
+        super.init(objectSchema: schema)
+    }
 }
